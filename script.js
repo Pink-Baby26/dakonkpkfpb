fcef595963d6c6d7915f410ecddc9d1b7ce79c28
Pink-Baby26 <62883995+Pink-Baby26@users.noreply.github.com>
2024-11-28 11:59:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -363,7 +363,8 @@ function nextstep() {
       title: 'Lanjut ke Perhitungan Selanjutnya',
       text: '',
       icon: 'info',
-      confirmButtonText: 'OK'
+      showConfirmButton: false,
+      timer: 1000
   }).then((result) => {
       if (result.isConfirmed) {
           // Reset grid
@@ -422,6 +423,13 @@ function checkBothCalculations() {
       nextButton.classList.remove('hidden'); // Tampilkan tombol
   }
 }
+
+function Menu(e){
+  let list = document.querySelector('ul');
+  e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100'), list.classList.add('z-40')) 
+  :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+  }
+
 
 
 
